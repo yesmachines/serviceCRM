518f1cb2dd1a72344ee81f67828a5ec39a8a4640
ipsgiraf <shainu.giraf@gmail.com>
2025-05-19 15:25:36 +0530

    config vars

---
+++ config/systemvars.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'installation_tf' => [
+        'inspection' => 'Inspection',
+        'to_quantity' => 'To Quantity',
+        'visual' => 'Visual',
+        'explanation_products' => 'Explanation On Products',
+        'installation' => 'Installation',
+        'startup_training' => 'Start up & Training',
+        'dos_donts' => 'Do\'s & Don\'ts',
+        'safety_measures' => 'Safety Measures',
+    ],
+    'installation_cf' => [
+        'punctuality' => 'Punctuality',
+        'knowledge_attitude' => 'Knowledge & Attitude',
+        'presentation_explanation' => 'Presentation & Explanation',
+        'overall_ratings' => 'Overall Ratings',
+    ],
+    'service_cf' => [
+        'punctuality' => 'Punctuality',
+        'knowledge_expertise' => 'Knowledge & Expertise',
+        'presentation_explanation' => 'Presentation & Explanation',
+        'problem_solving_efficiency' => 'Problem Solving & Efficiency',
+        'overall_satisfaction' => 'Overall Satisfaction',
+    ],
+];
